95ee13b88e946cdcb684f49fbf128ea74bd9b3c1
Charlie Hebert <charlie.hebert92@gmail.com>
2016-02-29 19:23:21 +0000

    Swap out http-server for express

--- src/server/server.js
+++ src/server/server.js
@@ -6,22 +6,18 @@
 (function() {
 	"use strict";
 
-	var httpServer = require("http-server");
+  var express = require("express");
+  var app = express();
+
 	var server;
 
 	exports.start = function(portNumber, dirToServe, callback) {
-		server = httpServer.createServer({
-			root: dirToServe
-		});
-		server.listen(portNumber, callback);
+    app.use(express.static(dirToServe));
+    server = app.listen(portNumber, callback);
 	};
 
 	exports.stop = function(callback) {
-		// Technically, we're poking into HttpServer's private data here. Bad us.
-    // The http-server module is just a placeholder, and it doesn't have a
-    // callback for server.close(). Bad http-server. So don't use this code.
-		// Write something better.
-		server.server.close(callback);
+		server.close(callback);
 	};
 
 }());
